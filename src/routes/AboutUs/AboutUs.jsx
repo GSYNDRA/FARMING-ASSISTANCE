a909bad927c103b5f58e70d598c6b71ab6d4c298
cyber141 <cyberzzz2k3@gmail.com>
2024-05-10 01:52:19 +0700

    func: update home-page (about us) without logic

--- src/routes/AboutUs/AboutUs.jsx
+++ src/routes/AboutUs/AboutUs.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import Header from "./Header";
 import Carousel from "./Carousel";
+import OurStory from "./OurStory";
+import OurBlog from "./OurBlog";
+import Footer from "./Footer";
+import Benefit from "./Benefit";
 
 const AboutUs = () => {
   return (
     <div
-      className="bg-white"
       style={{
-        background: "white",
+        background: "#EEE",
       }}
     >
       <Header />
       <Carousel />
+      <OurStory />
+      <OurBlog />
+      <Benefit />
+      <Footer />
     </div>
   );
 };
--- src/routes/AboutUs/Carousel.jsx
+++ src/routes/AboutUs/Carousel.jsx
@@ -1,17 +1,58 @@
 import React from "react";
+import carouselImg from "../../assets/carouselImg.png";
+import { NavLink } from "react-router-dom";
 
 const Carousel = () => {
   return (
-    <div className="h-[50.375rem] bg-[url('/src/assets/carouselImg.png')]">
+    <div>
       <div
-        className="relative"
         style={{
-          background: "w-full rgba(30, 30, 30, 0.60)",
-          content: "",
-          height: "100%",
+          filter: "brightness(100%)",
         }}
-      ></div>
-      <div className="absolute z-10">Hello</div>
+      >
+        <img
+          className="h-[45rem] w-full relative"
+          style={{
+            filter: "brightness(40%)",
+          }}
+          // src={`${carouselImg}`}
+          src={`${carouselImg}`}
+          alt=""
+        />
+        <div className="absolute top-0 left-0 text-white w-full text-center mx-auto mt-[6rem]">
+          <div
+            className="mx-auto h-fit"
+            style={{
+              fontSize: "5.625rem",
+              width: "75rem",
+              fontWeight: 700,
+              lineHeight: "5.85rem",
+            }}
+          >
+            The Role of Technology in Revolutionizing Agriculture
+          </div>
+          <div className="w-[40.625rem] text-center mx-auto my-[4rem]">
+            <span className="font-normal">
+              Li Europan lingues es membres del sam familie. Lor separat
+              existentie es un myth. Por scientie, musica, sport etc, litot
+              Europa usa li sam vocabular.
+            </span>
+          </div>
+
+          <div className=" flex mx-auto justify-center">
+            <div>
+              <NavLink className="bg-[#204E51] text-white px-8 py-3 rounded-lg mr-16">
+                Get Started
+              </NavLink>
+            </div>
+            <div>
+              <NavLink className="border text-white px-8 py-3 rounded-lg">
+                Learn More
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
