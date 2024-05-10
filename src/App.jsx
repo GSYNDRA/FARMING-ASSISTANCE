a3591a4a97a8591334eace1e8fbf627be88683ee
cyber141 <cyberzzz2k3@gmail.com>
2024-05-10 15:54:19 +0700

    func: update auth (login page)

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,25 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Layout";
 import AboutUs from "./routes/AboutUs/AboutUs";
+import AuthTemplate from "./routes/Auth/AuthTemplate";
+import Login from "./routes/Auth/Login";
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route index path="about-us" element={<AboutUs />} />
+        <Route index path="/" element={<AboutUs />} />
 
         {/* All main pages */}
         {/* <Route path="/" element={<Home />} /> */}
 
         {/* About */}
         {/* <Route path="/setting" element={<Setting />} /> */}
+
+        {/* Auth */}
+        <Route path="/auth" element={<AuthTemplate />}>
+          <Route path="login" element={<Login />} />
+        </Route>
 
         {/* Error pages */}
         {/* <Route path="*" element={<NotFound />} /> */}
