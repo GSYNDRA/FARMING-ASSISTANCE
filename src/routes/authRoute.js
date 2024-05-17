4a67180993ae224e0ae11ef37b3982e2bbc893e0
MinhMoi <ititiu21243@Student.hcmiu.edu.vn>
2024-05-17 10:18:53 +0700

    Login: fix API

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,11 +1,10 @@
-import express from 'express';
-import { login } from '../controllers/authController.js';
+import express from "express";
+import { login } from "../controllers/authController.js";
 
 const authRoute = express.Router();
 
 //login
-authRoute.post("/login/:roleID", login);
+authRoute.post("/login", login);
 //signup
-
 
 export default authRoute;
